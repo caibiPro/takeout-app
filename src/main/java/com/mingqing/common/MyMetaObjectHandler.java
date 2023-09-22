ccdcb049802b56d8e80605db78017b80a3454dc8
mingqing <tanmq1995@outlook.com>
2023-09-22 17:07:18 +0800

    修复createTime和updateTime字段的自动填充功能

--- src/main/java/com/mingqing/common/MyMetaObjectHandler.java
+++ src/main/java/com/mingqing/common/MyMetaObjectHandler.java
@@ -1,25 +1,25 @@
-package com.mingqing.config;
+package com.mingqing.common;
 
 import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.reflection.MetaObject;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 
 @Slf4j
-@Configuration
-public class FieldAutoFillConfig implements MetaObjectHandler {
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
 	@Override
 	public void insertFill(MetaObject metaObject) {
-		log.info("start insert fill ...");
+		log.info("start insert fill ....");
 		this.strictInsertFill(metaObject, "createTime", LocalDateTime.class, LocalDateTime.now());
-
+		this.strictInsertFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now());
 	}
 
 	@Override
 	public void updateFill(MetaObject metaObject) {
-		log.info("start update fill ...");
+		log.info("start update fill ....");
 		this.strictUpdateFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now());
 	}
 }
