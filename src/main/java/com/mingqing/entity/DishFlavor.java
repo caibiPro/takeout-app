60cda5efb107c4902c66c7d049308b680888a09a
mingqing <tanmq1995@outlook.com>
2023-09-26 17:30:24 +0800

    菜品管理三层架构搭建

---
+++ src/main/java/com/mingqing/entity/DishFlavor.java
@@ -0,0 +1,37 @@
+package com.mingqing.entity;
+
+import com.baomidou.mybatisplus.annotation.*;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import lombok.Data;
+
+@TableName(value ="dish_flavor")
+@Data
+public class DishFlavor implements Serializable {
+    @TableId("id")
+    private Long id;
+
+    private Long dishId;
+
+    private String name;
+
+    private String value;
+
+    @TableField(fill = FieldFill.INSERT)
+    private LocalDateTime createTime;
+
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private LocalDateTime updateTime;
+
+    @TableField(fill = FieldFill.INSERT)
+    private Long createUser;
+
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Long updateUser;
+
+    @TableLogic
+    private Integer isDeleted;
+
+    private static final long serialVersionUID = 1L;
+}
---
+++ src/main/java/com/mingqing/mapper/DishFlavorMapper.java
@@ -0,0 +1,18 @@
+package com.mingqing.mapper;
+
+import com.mingqing.entity.DishFlavor;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author mingqing
+* @description 针对表【dish_flavor(菜品口味关系表)】的数据库操作Mapper
+* @createDate 2023-09-26 17:28:44
+* @Entity com.mingqing.entity.DishFlavor
+*/
+public interface DishFlavorMapper extends BaseMapper<DishFlavor> {
+
+}
+
+
+
+
