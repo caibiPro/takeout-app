513ea3068adf8a34361586752ee532740fd0761e
mingqing <tanmq1995@outlook.com>
2023-09-26 10:28:35 +0800

    完成分类分页查询功能

--- src/main/java/com/mingqing/controlloer/CategoryController.java
+++ src/main/java/com/mingqing/controlloer/CategoryController.java
@@ -1,14 +1,14 @@
 package com.mingqing.controlloer;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.api.R;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.mingqing.common.utils.Result;
 import com.mingqing.entity.Category;
 import com.mingqing.service.CategoryService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -24,5 +24,14 @@ public class CategoryController {
 			return Result.error("添加失败");
 		}
 		return Result.success("添加成功");
+	}
+
+	@GetMapping("/page")
+	public Result<?> page(int page, int pageSize) {
+		Page<Category> pageInfo = new Page<>(page, pageSize);
+		LambdaQueryWrapper<Category> queryWrapper = new LambdaQueryWrapper<>();
+		queryWrapper.orderByAsc(Category::getSort);
+		categoryService.page(pageInfo, queryWrapper);
+		return Result.success(pageInfo);
 	}
 }
