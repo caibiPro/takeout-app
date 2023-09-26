56a587c15479f354b1747c84bba417f919706935
mingqing <tanmq1995@outlook.com>
2023-09-26 17:16:19 +0800

    新增下载功能

--- src/main/java/com/mingqing/controlloer/CommonController.java
+++ src/main/java/com/mingqing/controlloer/CommonController.java
@@ -3,13 +3,12 @@ package com.mingqing.controlloer;
 import com.mingqing.common.utils.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.UUID;
 
@@ -17,7 +16,7 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/common")
 public class CommonController {
-	@Value("${reggie.dump.base-path}")
+	@Value("${reggie.dump.dish.base-path}")
 	private String basePath;
 
 	@PostMapping("/upload")
@@ -36,5 +35,19 @@ public class CommonController {
 		uploadFile.transferTo(new File(basePath + filename));
 		log.info("转存文件至：{}", basePath + filename);
 		return Result.success(filename);
+	}
+
+	@GetMapping("/download")
+	public void download(String name, HttpServletResponse response) {
+		byte[] buffer = new byte[1024];
+		int bytesRead;
+		try {
+			FileInputStream fis = new FileInputStream(new File(basePath + name));
+			while ((bytesRead = fis.read(buffer)) != -1) {
+				response.getOutputStream().write(buffer, 0, bytesRead);
+			}
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
 	}
 }
